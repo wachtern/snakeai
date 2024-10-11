e035785ff3490d2bf72b2939802cab470faef37d
wachtern <wachternlvg@gmail.com>
2024-10-11 13:12:50 +0200

    feat(001): create playfield ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import "./App.css";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./providers/theme";
-import "./App.css";
+import Playfield from "./views/Playfield";
 
 function App() {
-  return <ThemeProvider theme={theme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Playfield />
+    </ThemeProvider>
+  );
 }
 
 export default App;
